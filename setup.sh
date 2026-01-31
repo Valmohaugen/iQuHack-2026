68e0d871389422a0c2bdd570e4ca8e5c0072b8c5
Nathan Jones <njones93531@gmail.com>
2026-01-31 12:39:13 -0500

    thing

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ conda create -n mit26 python=3.11
 conda activate mit26
 conda install pip
 pip install -r rmsynth/requirements.txt
+pip install qiskit
 
 python -m pip install -U pip setuptools wheel
 python -m pip install -e rmsynth/
+
